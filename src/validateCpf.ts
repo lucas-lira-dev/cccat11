b17e776cd710623800e6a4cacb48fd5a85292fdc
Lucas Lira <lucasrocha446@gmail.com>
2023-07-18 10:26:06 -0300

    feat: finish two lesson

--- src/validateCpf.ts
+++ src/validateCpf.ts
@@ -1,42 +1,31 @@
-const DIGIT_1_FACTOR = 10;
-const DIGIT_2_FACTOR = 11;
-
-export function validate(cpf: string) {
-  if (!cpf) return false;
-  cpf = removeNonDigits(cpf);
-  if (!isValidLength(cpf)) return false;
-  if (allDigitsTheSame(cpf)) return false;
-  const digit1 = calculateDigit(cpf, DIGIT_1_FACTOR);
-  const digit2 = calculateDigit(cpf, DIGIT_2_FACTOR);
-  let checkDigit = extractCheckDigit(cpf);
-  const calculatedCheckDigit = `${digit1}${digit2}`;
-  return checkDigit == calculatedCheckDigit;
+function isValidLength (cpf: string) {
+	return cpf.length !== 11;
 }
 
-function removeNonDigits(cpf: string) {
-  return cpf.replace(/\D+/g, "");
+function allDigitsTheSame (cpf: string) {
+	return cpf.split("").every(c => c === cpf[0]);
+};
+
+function removeNonDigits (cpf: string) {
+	return cpf.replace(/\D/g, "");
 }
 
-function isValidLength(cpf: string) {
-  return cpf.length === 11;
+function calculateDigit (cpf: string, factor: number) {
+	let total = 0;
+	for (const digit of cpf) {
+		if (factor > 1) total += parseInt(digit) * factor--;
+	}
+	const rest = total%11;
+	return (rest < 2) ? 0 : 11 - rest;
 }
 
-function allDigitsTheSame(cpf: string) {
-  const [firstDigit] = cpf;
-  return [...cpf].every((digit) => digit === firstDigit);
-}
-
-function calculateDigit(cpf: string, factor: number) {
-  let total = 0;
-  for (const digit of cpf) {
-    if (factor > 1) {
-      total += parseInt(digit) * factor--;
-    }
-  }
-  const rest = total % 11;
-  return rest < 2 ? 0 : 11 - rest;
-}
-
-function extractCheckDigit(cpf: string) {
-  return cpf.slice(-2);
+export function validate (cpf: string) {
+	cpf = removeNonDigits(cpf);
+	if (isValidLength(cpf)) return false;
+	if (allDigitsTheSame(cpf)) return false;
+	const dg1 = calculateDigit(cpf, 10);
+	const dg2 = calculateDigit(cpf, 11);
+	let actualCheckDigit = cpf.slice(9);
+	const calculatedCheckDigit = `${dg1}${dg2}`;
+	return actualCheckDigit === calculatedCheckDigit;
 }
--- test/validateCpf.test.ts
+++ test/validateCpf.test.ts
@@ -1,25 +1,29 @@
 import { validate } from "../src/validateCpf";
 
-test.each(["407.302.170-27", "684.053.160-00", "746.971.314-01"])(
-  "Deve testar um cpf válido %s",
-  function (cpf: string) {
-    const isValid = validate(cpf);
-    expect(isValid).toBeTruthy();
-  }
-);
 
-test.each(["406.302.170-27", "406.302.170", "406.302"])(
-  "Deve testar um cpf inválido %s",
-  function () {
-    const isValid = validate("610.051.823-60");
-    expect(isValid).toBeFalsy();
-  }
-);
+test.each([
+	"407.302.170-27",
+	"684.053.160-00",
+	"746.971.314-01"
+])("Deve testar um cpf válido %s", function (cpf: string) {
+	const isValid = validate(cpf);
+	expect(isValid).toBeTruthy();
+});
 
-test.each(["111.111.111-11", "222.222.222-22", "333.333.333-33"])(
-  "Deve testar um cpf com dígitos iguais %s",
-  function () {
-    const isValid = validate("610.051.823-60");
-    expect(isValid).toBeFalsy();
-  }
-);
+test.each([
+	"406.302.170-27",
+	"406.302.170",
+	"406.302",
+])("Deve testar um cpf inválido %s", function (cpf: string) {
+	const isValid = validate(cpf);
+	expect(isValid).toBeFalsy();
+});
+
+test.each([
+	"111.111.111-11",
+	"222.222.222-22",
+	"333.333.333-33"
+])("Deve testar um cpf com dígitos iguais %s", function (cpf: string) {
+	const isValid = validate(cpf);
+	expect(isValid).toBeFalsy();
+});
